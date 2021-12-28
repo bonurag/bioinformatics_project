795e4b6ea08880944ab4f0d9e7d19a93d55aa88b
Giuseppe Bonura <giuseppebonuraster@gmail.com>
2021-12-28 22:31:50 +0100

    Some modify on data analysis

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -7,6 +7,9 @@ from multiprocessing import cpu_count
 from ucsc_genomes_downloader import Genome
 from utils.bio_constants import GENOME_CACHE_DIR
 
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import RobustScaler
+
 import pandas as pd
 import numpy as np
 
@@ -14,7 +17,6 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping
 
 from typing import Dict, List, Tuple, Optional, Union
-
 
 from utils.data_processing import *
 
@@ -223,3 +225,50 @@ def to_bed(data: pd.DataFrame) -> pd.DataFrame:
 
 def get_genome() -> Genome:
     return Genome("hg38", cache_directory=GENOME_CACHE_DIR)
+
+"""Module providing normalization for epigenomic data."""
+from typing import Tuple
+import numpy as np
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+
+def normalize_epigenomic_data(
+    train_x: np.ndarray,
+    test_x: np.ndarray = None
+) -> Tuple[np.ndarray]:
+    """Return imputed and normalized epigenomic data.
+
+    We fit the imputation and normalization on the training data and
+    apply it to both the training data and the test data.
+
+    Parameters
+    -------------------------
+    train_x: np.ndarray,
+        Training data to use to fit the imputer and scaled.
+    test_x: np.ndarray = None,
+        Test data to be normalized.
+
+    Returns
+    -------------------------
+    Tuple with imputed and scaled train and test data.
+    """
+    # Create the imputer and scaler object
+    imputer = KNNImputer()
+    scaler = RobustScaler()
+    # Fit the imputer object
+    imputer.fit(train_x)
+    # Impute the train and test data
+    imputed_train_x = imputer.transform(train_x)
+    if test_x is not None:
+        imputed_test_x = imputer.transform(test_x)
+    # Fit the scaler object
+    scaler.fit(imputed_train_x)
+    # Scale the train and test data
+    scaled_train_x = scaler.transform(imputed_train_x)
+    if test_x is not None:
+        scaled_test_x = scaler.transform(imputed_test_x)
+    if test_x is not None:
+        # Return the normalized data
+        return scaled_train_x, scaled_test_x
+    return scaled_train_x
